600247e1cc563b35d8d82f9db45e637a1f32ccd7
Sajjad Sayadi <4110@persepolistile.net>
2025-07-03 12:12:47 +0330

    Add or update test1.py for Appwrite function

--- test1.py
+++ test1.py
@@ -1,13 +1,16 @@
 import json
 
-def main(req, res):
-    if req.method == "POST":
-        return res.json({
+def main(context):
+    print(f"Context: {context.__dict__}")
+    print(f"Request Method: {context.req.method}")
+    print(f"Request Body: {context.req.body}")
+    if context.req.method == "POST":
+        return context.res.json({
             "message": "درخواست POST بدون بدنه با موفقیت دریافت شد!",
             "status": "success"
         })
     else:
-        return res.json({
+        return context.res.json({
             "message": "فقط درخواست‌های POST پشتیبانی می‌شوند!",
             "status": "error"
         }, status=405)
